e8b28a1cfa2913694127d107c61d9b6b0e3c7b35
Nikola Mladenovic <nikola@mladenovic.biz>
2016-04-14 00:24:10 +0200

    Closes #24 Initial iOS Cocoa Touch framework setup, along with Alamofire dependency.

---
+++ Commercetools/Commercetools.h
@@ -0,0 +1,19 @@
+//
+//  Commercetools.h
+//  Commercetools
+//
+//  Created by Nikola Mladenovic on 4/13/16.
+//  Copyright © 2016 Commercetools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CommercetoolsSDK.
+FOUNDATION_EXPORT double CommercetoolsVersionNumber;
+
+//! Project version string for CommercetoolsSDK.
+FOUNDATION_EXPORT const unsigned char CommercetoolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Commercetools/PublicHeader.h>
+
+
