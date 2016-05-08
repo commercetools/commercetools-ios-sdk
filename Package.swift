e58fc8ebb5ebbc9a4f137562e093eb1faac1dbc3
Nikola Mladenovic <nikola@mladenovic.biz>
2016-05-08 13:04:36 +0200

    Closes #30 Added manifest file for Swift package manager

---
+++ Package.swift
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2016 Commercetools. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+name: "Commercetools",
+        dependencies: [
+                .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 3),
+        ]
+)
