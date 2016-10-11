fc94b2a52e2d6858019b4625f391bfcdc439fc8d
Nikola Mladenovic <nikola@mladenovic.biz>
2016-10-11 10:18:24 +0200

    Closes #67 Add product type and categories related models

--- Source/Endpoints/ProductProjection.swift
+++ Source/Endpoints/ProductProjection.swift
@@ -8,49 +8,49 @@ import ObjectMapper
 /**
     Provides complete set of interactions for querying, retrieving and creating an order.
 */
-open class ProductProjection: QueryEndpoint, ByIdEndpoint {
+open class ProductProjection: QueryEndpoint, ByIdEndpoint, Mappable {
     
-    public typealias ResponseType = NoMapping
+    public typealias ResponseType = ProductProjection
 
-    // MARK:: Properties
+    // MARK: - Properties
 
     open static let path = "product-projections"
 
-    // MARK:: Product projection endpoint functionality
+    // MARK: - Product projection endpoint functionality
 
     /**
         Full-text search queries over product projections endpoint.
 
-       : parameter staged:                   An optional bool value, determining whether to query
+        - parameter staged:                   An optional bool value, determining whether to query
                                               for the current or staged projections.
-       : parameter sort:                     An optional array of sort options used for sorting the results.
-       : parameter expansion:                An optional array of expansion property names.
-       : parameter limit:                    An optional parameter to limit the number of returned results.
-       : parameter offset:                   An optional parameter to set the offset of the first returned result.
-       : parameter lang:                     Locale for text search. If no locale is provided, current locale will be
+        - parameter sort:                     An optional array of sort options used for sorting the results.
+        - parameter expansion:                An optional array of expansion property names.
+        - parameter limit:                    An optional parameter to limit the number of returned results.
+        - parameter offset:                   An optional parameter to set the offset of the first returned result.
+        - parameter lang:                     Locale for text search. If no locale is provided, current locale will be
                                               the default value.
-       : parameter text:                     Localized text to analyze and search for.
-       : parameter fuzzy:                    An optional parameter determining whether to apply fuzzy search on
+        - parameter text:                     Localized text to analyze and search for.
+        - parameter fuzzy:                    An optional parameter determining whether to apply fuzzy search on
                                               the text to analyze.
-       : parameter filter:                   An optional filter to be applied to the search results
+        - parameter filter:                   An optional filter to be applied to the search results
                                               (after facets have been calculated).
-       : parameter filterQuery:              An optional filter to be applied to the search results
+        - parameter filterQuery:              An optional filter to be applied to the search results
                                               (before facets have been calculated).
-       : parameter filterFacets:             An optional filter for all facets calculations.
-       : parameter facets:                   An optional facets param used for calculating statistical counts
+        - parameter filterFacets:             An optional filter for all facets calculations.
+        - parameter facets:                   An optional facets param used for calculating statistical counts
                                               to aid in faceted navigation.
-       : parameter priceCurrency:            An optional currency code compliant to ISO 4217.
-       : parameter priceCountry:             An optional two-digit country code as per ISO 3166-1 alpha-2.
-       : parameter priceCustomerGroup:       An optional price customer group UUID.
-       : parameter priceChannel:             An optional price channel UUID.
-       : parameter result:                   The code to be executed after processing the response.
+        - parameter priceCurrency:            An optional currency code compliant to ISO 4217.
+        - parameter priceCountry:             An optional two-digit country code as per ISO 3166-1 alpha-2.
+        - parameter priceCustomerGroup:       An optional price customer group UUID.
+        - parameter priceChannel:             An optional price channel UUID.
+        - parameter result:                   The code to be executed after processing the response.
     */
     open static func search(staged: Bool? = nil, sort: [String]? = nil, expansion: [String]? = nil, limit: UInt? = nil,
-                              offset: UInt? = nil, lang: Locale = Locale.current, text: String? = nil,
-                              fuzzy: Bool? = nil, filter: String? = nil, filterQuery: String? = nil, filterFacets: String? = nil,
-                              facets: [String]? = nil, priceCurrency: String? = nil, priceCountry: String? = nil,
-                              priceCustomerGroup: String? = nil, priceChannel: String? = nil,
-                              result: @escaping (Result<ResponseType>) -> Void) {
+                            offset: UInt? = nil, lang: Locale = Locale.current, text: String? = nil,
+                            fuzzy: Bool? = nil, filter: String? = nil, filterQuery: String? = nil, filterFacets: String? = nil,
+                            facets: [String]? = nil, priceCurrency: String? = nil, priceCountry: String? = nil,
+                            priceCustomerGroup: String? = nil, priceChannel: String? = nil,
+                            result: @escaping (Result<ResponseType>) -> Void) {
 
         requestWithTokenAndPath(result, { token, path in
             let fullPath = pathWithExpansion("\(path)search", expansion: expansion)
@@ -63,27 +63,27 @@ open class ProductProjection: QueryEndpoint, ByIdEndpoint {
                     priceChannel: priceChannel, params: parameters)
 
             Alamofire.request(fullPath, parameters: parameters, encoding: URLEncoding.queryString, headers: self.headers(token))
-            .responseJSON(queue: DispatchQueue.global(), completionHandler: { response in
-                handleResponse(response, result: result)
-            })
+                    .responseJSON(queue: DispatchQueue.global(), completionHandler: { response in
+                        handleResponse(response, result: result)
+                    })
         })
     }
 
     /**
         Retrieves suggestions based on the `searchKeyword` product field.
 
-       : parameter staged:                   An optional bool value, determining whether to query
+        - parameter staged:                   An optional bool value, determining whether to query
                                               for the current or staged projections.
-       : parameter limit:                    An optional parameter to limit the number of returned results.
-       : parameter lang:                     Locale for text search. If no locale is provided, current locale will be
+        - parameter limit:                    An optional parameter to limit the number of returned results.
+        - parameter lang:                     Locale for text search. If no locale is provided, current locale will be
                                               the default value.
-       : parameter searchKeywords:           Localized text to analyze retrieve suggestions for.
-       : parameter fuzzy:                    An optional parameter determining whether to apply fuzzy search on
+        - parameter searchKeywords:           Localized text to analyze retrieve suggestions for.
+        - parameter fuzzy:                    An optional parameter determining whether to apply fuzzy search on
                                               the text to analyze.
-       : parameter result:                   The code to be executed after processing the response.
+        - parameter result:                   The code to be executed after processing the response.
     */
     open static func suggest(staged: Bool? = nil, limit: UInt? = nil, lang: Locale = Locale.current,
-                               searchKeywords: String, fuzzy: Bool? = nil, result: @escaping (Result<ResponseType>) -> Void) {
+                             searchKeywords: String, fuzzy: Bool? = nil, result: @escaping (Result<ResponseType>) -> Void) {
 
         requestWithTokenAndPath(result, { token, path in
             var parameters = paramsWithStaged(staged, params: queryParameters(limit: limit))
@@ -91,26 +91,26 @@ open class ProductProjection: QueryEndpoint, ByIdEndpoint {
             parameters["searchKeywords." + lang.identifier.replacingOccurrences(of: "_", with: "-")] = searchKeywords as NSString
 
             Alamofire.request("\(path)suggest", parameters: parameters, encoding: URLEncoding.queryString, headers: self.headers(token))
-            .responseJSON(queue: DispatchQueue.global(), completionHandler: { response in
-                handleResponse(response, result: result)
-            })
+                    .responseJSON(queue: DispatchQueue.global(), completionHandler: { response in
+                        handleResponse(response, result: result)
+                    })
         })
     }
 
     /**
         Queries for product projection.
 
-       : parameter staged:                   An optional bool value, determining whether to query
+        - parameter staged:                   An optional bool value, determining whether to query
                                               for the current or staged projections.
-       : parameter predicate:                An optional array of predicates used for querying for  product projections.
-       : parameter sort:                     An optional array of sort options used for sorting the results.
-       : parameter expansion:                An optional array of expansion property names.
-       : parameter limit:                    An optional parameter to limit the number of returned results.
-       : parameter offset:                   An optional parameter to set the offset of the first returned result.
-       : parameter result:                   The code to be executed after processing the response.
+        - parameter predicate:                An optional array of predicates used for querying for  product projections.
+        - parameter sort:                     An optional array of sort options used for sorting the results.
+        - parameter expansion:                An optional array of expansion property names.
+        - parameter limit:                    An optional parameter to limit the number of returned results.
+        - parameter offset:                   An optional parameter to set the offset of the first returned result.
+        - parameter result:                   The code to be executed after processing the response.
     */
     open static func query(staged: Bool? = nil, predicates: [String]? = nil, sort: [String]? = nil, expansion: [String]? = nil,
-                      limit: UInt? = nil, offset: UInt? = nil, result: @escaping (Result<ResponseType>) -> Void) {
+                           limit: UInt? = nil, offset: UInt? = nil, result: @escaping (Result<ResponseType>) -> Void) {
 
         requestWithTokenAndPath(result, { token, path in
             let fullPath = pathWithExpansion(path, expansion: expansion)
@@ -118,20 +118,20 @@ open class ProductProjection: QueryEndpoint, ByIdEndpoint {
                     limit: limit, offset: offset))
 
             Alamofire.request(fullPath, parameters: parameters, encoding: URLEncoding.queryString, headers: self.headers(token))
-            .responseJSON(queue: DispatchQueue.global(), completionHandler: { response in
-                handleResponse(response, result: result)
-            })
+                    .responseJSON(queue: DispatchQueue.global(), completionHandler: { response in
+                        handleResponse(response, result: result)
+                    })
         })
     }
 
     /**
         Retrieves a product projection by UUID.
 
-       : parameter staged:                   An optional bool value, determining whether to retrieve current or staged
+        - parameter staged:                   An optional bool value, determining whether to retrieve current or staged
                                               product projection variant.
-       : parameter id:                       Unique ID of the product projection to be retrieved.
-       : parameter expansion:                An optional array of expansion property names.
-       : parameter result:                   The code to be executed after processing the response.
+        - parameter id:                       Unique ID of the product projection to be retrieved.
+        - parameter expansion:                An optional array of expansion property names.
+        - parameter result:                   The code to be executed after processing the response.
     */
     open static func byId(_ id: String, staged: Bool? = nil, expansion: [String]? = nil, result: @escaping (Result<ResponseType>) -> Void) {
 
@@ -139,9 +139,9 @@ open class ProductProjection: QueryEndpoint, ByIdEndpoint {
             let fullPath = pathWithExpansion(path + id, expansion: expansion)
 
             Alamofire.request(fullPath, parameters: paramsWithStaged(staged), encoding: URLEncoding.queryString, headers: self.headers(token))
-            .responseJSON(queue: DispatchQueue.global(), completionHandler: { response in
-                handleResponse(response, result: result)
-            })
+                    .responseJSON(queue: DispatchQueue.global(), completionHandler: { response in
+                        handleResponse(response, result: result)
+                    })
         })
     }
 
@@ -199,7 +199,7 @@ open class ProductProjection: QueryEndpoint, ByIdEndpoint {
         reviewRatingStatistics  <- map["reviewRatingStatistics"]
     }
 
-    // MARK:: Helpers
+    // MARK: - Helpers
 
     private static func paramsWithStaged(_ staged: Bool?, params: [String: Any]? = nil) -> [String: Any] {
         var params = params ?? [String: Any]()
