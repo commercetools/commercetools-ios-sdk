03e1a653432961ca51970208bd04d4f3d06b5015
Nikola Mladenovic <nikola@mladenovic.biz>
2016-12-06 12:19:36 -0800

    Updated DeleteEndpointTests assertions

--- Tests/Core/DeleteEndpointTests.swift
+++ Tests/Core/DeleteEndpointTests.swift
@@ -36,8 +36,9 @@ class DeleteEndpointTests: XCTestCase {
         TestCart.create(["currency": "EUR"], result: { result in
             if let response = result.json, let id = response["id"] as? String, let version = response["version"] as? UInt, result.isSuccess {
                 TestCart.delete(id, version: version, result: { result in
-                    if let response = result.json, let deletedId = response["id"] as? String, result.isSuccess
-                            && deletedId == id {
+                    if let response = result.json, let deletedId = response["id"] as? String {
+                        XCTAssert(result.isSuccess)
+                        XCTAssertEqual(deletedId, id)
                         deleteExpectation.fulfill()
                     }
                 })
@@ -59,8 +60,11 @@ class DeleteEndpointTests: XCTestCase {
         TestCart.create(["currency": "EUR"], result: { result in
             if let response = result.json, let id = response["id"] as? String, let version = response["version"] as? UInt, result.isSuccess {
                 TestCart.delete(id, version: version + 1, result: { result in
-                    if let error = result.errors?.first as? CTError, result.statusCode == 409, case .concurrentModificationError(let reason, let currentVersion) = error,
-                            reason.message == "Object \(id) has a different version than expected. Expected: 2 - Actual: 1." && version == currentVersion {
+                    if let error = result.errors?.first as? CTError, case .concurrentModificationError(let reason, let currentVersion) = error {
+                        XCTAssert(result.isFailure)
+                        XCTAssertEqual(result.statusCode, 409)
+                        XCTAssertEqual(reason.message, "Object \(id) has a different version than expected. Expected: 2 - Actual: 1.")
+                        XCTAssertEqual(version, currentVersion)
                         deleteExpectation.fulfill()
                     }
                 })
@@ -80,13 +84,14 @@ class DeleteEndpointTests: XCTestCase {
         AuthManager.sharedInstance.loginCustomer(username: username, password: password, completionHandler: { _ in})
 
         TestCart.delete("cddddddd-ffff-4b44-b5b0-004e7d4bc2dd", version: 1, result: { result in
-            if let error = result.errors?.first as? CTError, result.statusCode == 404, case .resourceNotFoundError(let reason) = error,
-                    reason.message == "The Cart with ID 'cddddddd-ffff-4b44-b5b0-004e7d4bc2dd' was not found." {
+            if let error = result.errors?.first as? CTError, case .resourceNotFoundError(let reason) = error {
+                XCTAssert(result.isFailure)
+                XCTAssertEqual(result.statusCode, 404)
+                XCTAssertEqual(reason.message, "The Cart with ID 'cddddddd-ffff-4b44-b5b0-004e7d4bc2dd' was not found.")
                 deleteExpectation.fulfill()
             }
         })
 
         waitForExpectations(timeout: 10, handler: nil)
     }
-
 }
