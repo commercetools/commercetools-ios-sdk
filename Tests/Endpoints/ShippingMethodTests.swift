3c979cb9a59ffa8ec37ef1419a692c8c4b76a5b3
Nikola Mladenovic <nikola@mladenovic.biz>
2017-03-13 16:35:48 +0100

    Closes #126 Add ShippingMethod tests

---
+++ Tests/Endpoints/ShippingMethodTests.swift
@@ -0,0 +1,114 @@
+//
+// Copyright (c) 2016 Commercetools. All rights reserved.
+//
+
+import XCTest
+@testable import Commercetools
+
+class ShippingMethodTests: XCTestCase {
+
+    private class CreateShippingMethod: CreateEndpoint {
+        typealias ResponseType = ShippingMethod
+        typealias RequestDraft = NoMapping
+        static let path = "shipping-methods"
+    }
+
+    private class CreateZone: CreateEndpoint {
+        typealias ResponseType = Zone
+        typealias RequestDraft = NoMapping
+        static let path = "zones"
+    }
+
+    private class QueryTaxCategory: QueryEndpoint {
+        typealias ResponseType = TaxCategory
+        typealias RequestDraft = NoMapping
+        static let path = "tax-categories"
+    }
+
+    override func setUp() {
+        super.setUp()
+
+        setupProjectManagementConfiguration()
+        let semaphore = DispatchSemaphore(value: 0)
+        AuthManager.sharedInstance.token { token, error in
+            ShippingMethod.query(limit: 1) { result in
+                if result.model!.count == 0 {
+                    QueryTaxCategory.query(limit: 1) { taxCategoryResult in
+                        CreateZone.create(["name": "Germany", "locations": [["country": "DE"]]]) { zoneResult in
+                            CreateShippingMethod.create(["name": "test-shipping-method", "isDefault": true,
+                                                         "zoneRates":[["zone": ["id": zoneResult.model!.id!, "typeId": "zone"],
+                                                                       "shippingRates": [["price": ["currencyCode": "EUR", "centAmount": 1000]]]]],
+                                                         "taxCategory": ["id": taxCategoryResult.model!.results!.first!.id!,
+                                                                         "typeId": "tax-category"]]) { result in
+                                semaphore.signal()
+                            }
+                        }
+                    }
+                } else {
+                    semaphore.signal()
+                }
+            }
+        }
+        _ = semaphore.wait(timeout: DispatchTime.distantFuture)
+
+        cleanPersistedTokens()
+        setupTestConfiguration()
+    }
+
+    override func tearDown() {
+        cleanPersistedTokens()
+        super.tearDown()
+    }
+
+    func testQueryForShippingMethods() {
+        let shippingMethodsExpectation = expectation(description: "shipping methods expectation")
+
+        ShippingMethod.query(predicates: ["name=\"test-shipping-method\""], limit: 1) { result in
+            XCTAssert(result.isSuccess)
+            XCTAssertEqual(result.model?.results?.first?.name, "test-shipping-method")
+            shippingMethodsExpectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testShippingMethodsForCountry() {
+        let shippingMethodsExpectation = expectation(description: "shipping methods expectation")
+
+        ShippingMethod.for(country: "DE") { result in
+            XCTAssert(result.isSuccess)
+            XCTAssertEqual(result.model?.filter({ $0.name == "test-shipping-method" }).count, 1)
+            shippingMethodsExpectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testShippingMethodsForCart() {
+        let shippingMethodsExpectation = expectation(description: "shipping methods expectation")
+
+        let username = "swift.sdk.test.user2@commercetools.com"
+        let password = "password"
+
+        AuthManager.sharedInstance.loginCustomer(username: username, password: password, completionHandler: { _ in})
+
+        var cartDraft = CartDraft()
+        cartDraft.currency = "EUR"
+        var address = Address()
+        address.country = "DE"
+        cartDraft.shippingAddress = address
+
+        Cart.create(cartDraft, result: { result in
+            if let cart = result.model {
+                XCTAssert(result.isSuccess)
+                ShippingMethod.for(cart: cart) { result in
+                    XCTAssert(result.isSuccess)
+                    XCTAssertEqual(result.model?.filter({ $0.name == "test-shipping-method" }).count, 1)
+                    shippingMethodsExpectation.fulfill()
+                }
+            }
+        })
+
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+}
