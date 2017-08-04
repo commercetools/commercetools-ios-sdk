bc23daddbe5d572b29b763b3c23f01245bc00b42
Nikola Mladenovic <nikola@mladenovic.biz>
2017-08-04 13:15:30 +0200

    Removed Alamofire dependency for SPM

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ import PackageDescription
 let package = Package(
     name: "Commercetools",
     dependencies : [
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4),
         .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 2),
     ],
     exclude: ["Tests"]
