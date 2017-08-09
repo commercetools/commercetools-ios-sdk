b2aa75ab83c4b7e053c99d7494080fcf2f3e660d
Nikola Mladenovic <nikola@mladenovic.biz>
2017-08-09 14:58:22 +0200

    Remove ObjectMapper from active cart, category, graphql, update endpoint

--- Source/GraphQL.swift
+++ Source/GraphQL.swift
@@ -8,11 +8,11 @@ import Foundation
     GraphQL endpoint implementation, providing the ability to query for data with queries, passing optional variables and
     operation name. Currently, providing only raw JSON response (dictionary representation).
 */
-open class GraphQL: Endpoint {
+public struct GraphQL: Endpoint {
 
     public typealias ResponseType = NoMapping
 
-    open static let path = "graphql"
+    public static let path = "graphql"
 
     /**
         Executes GraphQL query, providing result in raw JSON format (dictionary representation).
@@ -22,7 +22,7 @@ open class GraphQL: Endpoint {
         - parameter operationName:            Operation name, in case there are several defined in the query.
         - parameter result:                   The code to be executed after processing the response.
     */
-    open static func query(_ query: String, variables: [String: Any]? = nil, operationName: String? = nil, result: @escaping (Result<ResponseType>) -> Void) {
+    public static func query(_ query: String, variables: [String: Any]? = nil, operationName: String? = nil, result: @escaping (Result<ResponseType>) -> Void) {
         requestWithTokenAndPath(result, { token, path in
             var parameters: [String: Any] = ["query": query]
             if let variables = variables {
