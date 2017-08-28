630ddd1321adf5401aaa54c78fe352f6a26944fa
Nikola Mladenovic <nikola@mladenovic.biz>
2017-08-28 11:16:36 +0200

    Update tests for anonymous customer

--- Tests/Endpoints/ShoppingListTests.swift
+++ Tests/Endpoints/ShoppingListTests.swift
@@ -46,6 +46,11 @@ class ShoppingListTests: XCTestCase {
     func testShoppingListRetrievalById() {
         let listRetrievalExpectation = expectation(description: "list retrieval expectation")
 
+        let username = "swift.sdk.test.user2@commercetools.com"
+        let password = "password"
+
+        AuthManager.sharedInstance.loginCustomer(username: username, password: password, completionHandler: { _ in})
+
         let listDraft = ShoppingListDraft(name: ["en": "testName"], deleteDaysAfterLastModification: 1)
 
         ShoppingList.create(listDraft, result: { result in
@@ -94,6 +99,94 @@ class ShoppingListTests: XCTestCase {
     }
 
     func testDeleteShoppingList() {
+        let listRemovalExpectation = expectation(description: "list removal expectation")
+
+        let username = "swift.sdk.test.user2@commercetools.com"
+        let password = "password"
+
+        AuthManager.sharedInstance.loginCustomer(username: username, password: password, completionHandler: { _ in})
+
+        let listDraft = ShoppingListDraft(name: ["en": "testName"])
+
+        ShoppingList.create(listDraft, result: { result in
+            if let createdList = result.model {
+                ShoppingList.delete(createdList.id, version: createdList.version) { result in
+                    XCTAssert(result.isSuccess)
+                    XCTAssertEqual(result.model!.id, createdList.id)
+                    listRemovalExpectation.fulfill()
+                }
+            }
+        })
+
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testShoppingListCreationWithAnonymous() {
+        let listCreationExpectation = expectation(description: "list creation expectation")
+
+        sampleLineItemDraft { productId, variantId in
+            let listDraft = ShoppingListDraft(name: ["en": "testName"], description: ["en": "testDesc"], lineItems: [ShoppingListDraft.LineItemDraft(productId: productId, variantId: variantId)], textLineItems: [TextLineItemDraft(name: ["en": "testName"])], deleteDaysAfterLastModification: 1)
+
+            ShoppingList.create(listDraft, result: { result in
+                if let shoppingList = result.model {
+                    XCTAssert(result.isSuccess)
+                    XCTAssertEqual(shoppingList.name, ["en": "testName"])
+                    XCTAssertEqual(shoppingList.textLineItems.first!.name, ["en": "testName"])
+                    XCTAssertEqual(shoppingList.lineItems.first!.productId, productId)
+                    listCreationExpectation.fulfill()
+                }
+            })
+        }
+
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testShoppingListRetrievalByIdWithAnonymous() {
+        let listRetrievalExpectation = expectation(description: "list retrieval expectation")
+
+        let listDraft = ShoppingListDraft(name: ["en": "testName"], deleteDaysAfterLastModification: 1)
+
+        ShoppingList.create(listDraft, result: { result in
+            if let createdList = result.model {
+                ShoppingList.byId(createdList.id) { result in
+                    XCTAssert(result.isSuccess)
+                    XCTAssertEqual(result.model!.id, createdList.id)
+                    listRetrievalExpectation.fulfill()
+                }
+            }
+        })
+
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testUpdateEndpointWithModelActionWithAnonymous() {
+        let updateExpectation = expectation(description: "update expectation")
+
+        sampleLineItemDraft { productId, variantId in
+            let listDraft = ShoppingListDraft(name: ["en": "testName"], description: ["en": "testDesc"], lineItems: [ShoppingListDraft.LineItemDraft(productId: productId, variantId: variantId)], deleteDaysAfterLastModification: 1)
+
+            ShoppingList.create(listDraft, result: { result in
+                if let shoppingList = result.model {
+                    XCTAssert(result.isSuccess)
+                    XCTAssertEqual(shoppingList.name, ["en": "testName"])
+                    XCTAssertEqual(shoppingList.lineItems.first!.productId, productId)
+                    let updateActions: [ShoppingListUpdateAction] = [.removeLineItem(lineItemId: shoppingList.lineItems.first!.id, quantity: nil), .changeName(name: ["en": "updatedName"])]
+                    ShoppingList.update(shoppingList.id, actions: UpdateActions(version: shoppingList.version, actions: updateActions)) { result in
+                        if let shoppingList = result.model {
+                            XCTAssert(result.isSuccess)
+                            XCTAssertEqual(shoppingList.name, ["en": "updatedName"])
+                            XCTAssertEqual(shoppingList.textLineItems.count, 0)
+                            updateExpectation.fulfill()
+                        }
+                    }
+                }
+            })
+        }
+
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+
+    func testDeleteShoppingListWithAnonymous() {
         let listRemovalExpectation = expectation(description: "list removal expectation")
 
         let listDraft = ShoppingListDraft(name: ["en": "testName"])
