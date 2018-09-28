e59a815a6f7cb1c636419daad74c72f40f8204ba
Nikola Mladenovic <nikola@mladenovic.biz>
2018-09-28 14:04:00 +0200

    Add ability to create application/x-www-form-urlencoded URLRequests

--- Source/Endpoint.swift
+++ Source/Endpoint.swift
@@ -141,7 +141,10 @@ public extension Endpoint {
         }
     }
 
-    static func request(url: String, method: HTTPMethod = .get, urlParameters: [String: String] = [:], json: [String: Any]? = nil, headers: [String: String] = [:]) -> URLRequest {
+    static func request(url: String, method: HTTPMethod = .get, urlParameters: [String: String] = [:], json: [String: Any]? = nil, formParameters: [String: String]? = nil, headers: [String: String] = [:]) -> URLRequest {
+        guard json == nil || formParameters == nil else {
+            fatalError("Cannot use both json and formParameters in the same URLRequest")
+        }
         var queryItems = [URLQueryItem]()
         urlParameters.forEach {
             queryItems.append(URLQueryItem(name: $0.key, value: $0.value))
@@ -151,10 +154,13 @@ public extension Endpoint {
         if let json = json, let data = try? JSONSerialization.data(withJSONObject: json, options: []) {
             jsonData = data
         }
-        return request(url: url, method: method, queryItems: queryItems, json: jsonData, headers: headers)
+        return request(url: url, method: method, queryItems: queryItems, json: jsonData, formParameters: formParameters, headers: headers)
     }
 
-    static func request(url: String, method: HTTPMethod = .get, queryItems: [URLQueryItem], json: Data? = nil, headers: [String: String] = [:]) -> URLRequest {
+    static func request(url: String, method: HTTPMethod = .get, queryItems: [URLQueryItem], json: Data? = nil, formParameters: [String: String]? = nil, headers: [String: String] = [:]) -> URLRequest {
+        guard json == nil || formParameters == nil else {
+            fatalError("Cannot use both json and formParameters in the same URLRequest")
+        }
         var urlComponents = URLComponents(string: url)!
         urlComponents.queryItems = queryItems + (urlComponents.queryItems ?? [])
 
@@ -164,9 +170,13 @@ public extension Endpoint {
         headers.forEach {
             urlRequest.setValue($0.value, forHTTPHeaderField: $0.key)
         }
+        urlRequest.setValue(json != nil ? "application/json" : "application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
 
         if let json = json {
             urlRequest.httpBody = json
+        } else if let formParameters = formParameters, !formParameters.isEmpty {
+            let body = formParameters.map({ "\($0.addingPercentEncoding(withAllowedCharacters: CharacterSet.alphanumerics) ?? "")=\($1.addingPercentEncoding(withAllowedCharacters: CharacterSet.alphanumerics) ?? "")" }).joined(separator: "&")
+            urlRequest.httpBody = body.data(using: .utf8)
         }
 
         return urlRequest
