3cf09f6e1636d983444e19f786cd6682387eef8f
Nikola Mladenovic <nikola@mladenovic.biz>
2019-06-13 17:12:39 +0200

    Move ActiveCart to a common cart file

--- Source/Cart.swift
+++ Source/Cart.swift
@@ -60,3 +60,31 @@ public struct Cart: QueryEndpoint, ByIdEndpoint, CreateEndpoint, UpdateEndpoint,
     public let lastModifiedBy: LastModifiedBy?
     public let itemShippingAddresses: [Address]
 }
+
+/**
+    Provides access to active cart endpoint.
+*/
+struct ActiveCart: Endpoint {
+
+    public typealias ResponseType = Cart
+
+    static let path = "me/active-cart"
+
+    /**
+     Retrieves the cart with state Active which has the most recent lastModifiedAt.
+
+     - parameter expansion:                An optional array of expansion property names.
+     - parameter result:                   The code to be executed after processing the response.
+     */
+    static func get(expansion: [String]? = nil, result: @escaping (Result<ResponseType>) -> Void) {
+
+        requestWithTokenAndPath(result, { token, path in
+            let fullPath = pathWithExpansion(path, expansion: expansion)
+            let request = self.request(url: fullPath, headers: self.headers(token))
+
+            perform(request: request) { (response: Result<ResponseType>) in
+                result(response)
+            }
+        })
+    }
+}
